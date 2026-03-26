580a269f2db1b53523173125daacde8ac9cf71a2
Dev_al <alorcilla11@icloud.com>
2026-03-26 15:45:40 +0800

    Fixed admin url typo

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.admin_urls),
+    path('admin/', admin.site.urls),
     path('', include('petal.urls')), # This connects everything
 ]
